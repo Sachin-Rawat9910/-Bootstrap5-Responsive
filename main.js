a5b27365f127b9b148c4605191bdf8483b10aa19
Sachin Rawat <111413393+Sachin-Rawat9910@users.noreply.github.com>
2022-09-27 19:29:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("#project-slider").owlCarousel({
+      loop: true,
+      margin: 0,
+      nav: false,
+      dots: false,
+      smartSpeed: 1000,
+      responsive: {
+        0: {},
+        768: {
+          items: 2,
+        },
+        1140: {
+          items: 2,
+        },
+      },
+    });
+  });
